efff7765e0ae7cb12c620c23939dd6f696239885
a0preetham <a0preetham@gmail.com>
2020-09-04 17:07:21 -0400

    Initial commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# angular-cdk-demo-virtual-scroll-4d1yee
+
+[Edit on StackBlitz ⚡️](https://stackblitz.com/edit/angular-cdk-demo-virtual-scroll-4d1yee)
---
+++ app/virtual-scroll/virtual-scroll.component.ts
@@ -0,0 +1,32 @@
+import { Component, OnInit } from '@angular/core';
+import {
+         ViewChild,
+         AfterViewInit } from '@angular/core';
+import { Observable } from 'rxjs';
+import { BehaviorSubject } from 'rxjs';
+import { CdkVirtualScrollViewport } from '@angular/cdk-experimental';
+
+
+@Component({
+  selector: 'app-virtual-scroll',
+  templateUrl: './virtual-scroll.component.html',
+  styleUrls: ['./virtual-scroll.component.css']
+})
+export class VirtualScrollComponent {
+
+  @ViewChild(CdkVirtualScrollViewport)
+  cdkVirtualScrollViewport: CdkVirtualScrollViewport;
+
+  items = [];
+
+
+  constructor(){
+    this.items = new Array(1000).fill(0).map((_,idx)=>idx);
+  }
+
+  trackBy(idx, item) {
+    console.log(idx)
+    return item.id;
+  }
+}
+
